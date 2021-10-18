e20bfb810b74df31d81de5376e96efde5e406cb0
ksks2012 <st890751999@gmail.com>
2021-10-18 22:15:35 +0800

    feat(tag): add grpc server

---
+++ server/tag.go
@@ -0,0 +1,26 @@
+import (
+	pb "github.co/tag-service/proto"
+	...
+)
+
+type TagServer struct {}
+
+func NewTagServer() *TagServer {
+	return &TagServer{}
+}
+
+func (t *TagServer) GetTagList(ctx context.Context, r *pb.GetTagListRequest) (*pb.GetTagListReply, error) {
+	api := bapi.NewAPI("http://127.0.0.1:18080")
+	body, err := api.GetTagList(ctx, r.GetName())
+	if err != nil {
+		return nil, err
+	}
+
+	tagList := pb.GetTagListReply{}
+	err = json.Unmarshal(body, &tagList)
+	if err != nil {
+		return nil, errcode.TogRPCError(errcode.Fail)
+	}
+
+	return &tagList, nil
+}
